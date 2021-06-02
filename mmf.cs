11f1d6536cbcffd1de57c936e4bf4ce83c3c97e5
jakubliszkowski <33127104+jakubliszkowski@users.noreply.github.com>
2021-06-02 13:38:35 +0200

    CreateorOpen struct size

--- mmf.cs
+++ mmf.cs
@@ -7,13 +7,14 @@ namespace WW1MMF
     struct Telemetry
     {
         public float Roll, Pitch, Heave, Yaw, Sway, Surge;
+        public const int Size = 6 * 4;
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            using(var mmf = MemoryMappedFile.OpenExisting("ww1"))
+            using(var mmf = MemoryMappedFile.CreateOrOpen("ww1", Telemetry.Size))
             {
                 using(var accessor = mmf.CreateViewAccessor())
                 {
@@ -21,7 +22,7 @@ namespace WW1MMF
                     {
                         accessor.Read(0, out Telemetry t);
                         Console.WriteLine($"Roll {t.Roll} Pitch {t.Pitch} Heave {t.Heave} Yaw {t.Yaw} Sway {t.Sway} Surge {t.Surge}");
-                        Thread.Sleep(20);
+                        Thread.Sleep(100);
                     }
                 }
             }
